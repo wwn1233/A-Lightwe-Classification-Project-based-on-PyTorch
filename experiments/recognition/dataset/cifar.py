93045c38458a8839ef706d2807b67035b7eec396
WWN <weinong.wang@hotmail.com>
2019-05-27 20:59:19 +0800

    fix some small bugs

--- experiments/recognition/dataset/cifar.py
+++ experiments/recognition/dataset/cifar.py
@@ -27,51 +27,96 @@ _imagenet_pca = {
     ])
 }
 
-class RandomErasing(object):
-    """ Randomly selects a rectangle region in an image and erases its pixels.
-        'Random Erasing Data Augmentation' by Zhong et al.
-        See https://arxiv.org/pdf/1708.04896.pdf
-    Args:
-         probability: The probability that the Random Erasing operation will be performed.
-         sl: Minimum proportion of erased area against input image.
-         sh: Maximum proportion of erased area against input image.
-         r1: Minimum aspect ratio of erased area.
-         mean: Erasing value.
-    """
+# class RandomErasing(object):
+#     """ Randomly selects a rectangle region in an image and erases its pixels.
+#         'Random Erasing Data Augmentation' by Zhong et al.
+#         See https://arxiv.org/pdf/1708.04896.pdf
+#     Args:
+#          probability: The probability that the Random Erasing operation will be performed.
+#          sl: Minimum proportion of erased area against input image.
+#          sh: Maximum proportion of erased area against input image.
+#          r1: Minimum aspect ratio of erased area.
+#          mean: Erasing value.
+#     """
 
-    def __init__(self, probability=0.5, sl=0.02, sh=0.2, r1=0.3, mean=[0.4914, 0.4822, 0.4465]):
+#     def __init__(self, probability=0.5, sl=0.02, sh=0.4, r1=0.3, mean=[0.4914, 0.4822, 0.4465]):
+#         self.probability = probability
+#         self.mean = mean
+#         self.sl = sl
+#         self.sh = sh
+#         self.r1 = r1
+
+#     def __call__(self, img):
+#         random_value = np.random.random()  # random.random()
+#         # print(random_value)
+#         # print(random_value)
+#         if random_value > self.probability:
+#             return img
+#         # count = 0
+#         for attempt in range(100):
+#             area = img.size()[1] * img.size()[2]
+
+#             target_area = np.random.uniform(self.sl, self.sh) * area
+#             aspect_ratio = np.random.uniform(self.r1, 1 / self.r1)
+
+#             h = int(round(math.sqrt(target_area * aspect_ratio)))
+#             w = int(round(math.sqrt(target_area / aspect_ratio)))
+
+#             if w < img.size()[2] and h < img.size()[1]:
+#                 # count +=1
+#                 x1 = np.random.randint(0, img.size()[1] - h)
+#                 y1 = np.random.randint(0, img.size()[2] - w)
+#                 if img.size()[0] == 3:
+#                     img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
+#                     img[1, x1:x1 + h, y1:y1 + w] = self.mean[1]
+#                     img[2, x1:x1 + h, y1:y1 + w] = self.mean[2]
+#                 else:
+#                     img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
+#                 return img
+#         return img
+class RandomErasing(object):
+    '''
+    Class that performs Random Erasing in Random Erasing Data Augmentation by Zhong et al. 
+    -------------------------------------------------------------------------------------
+    probability: The probability that the operation will be performed.
+    sl: min erasing area
+    sh: max erasing area
+    r1: min aspect ratio
+    mean: erasing value
+    -------------------------------------------------------------------------------------
+    '''
+    def __init__(self, probability = 0.5, sl = 0.02, sh = 0.4, r1 = 0.3, mean=[0.4914, 0.4822, 0.4465]):
         self.probability = probability
         self.mean = mean
         self.sl = sl
         self.sh = sh
         self.r1 = r1
-
+       
     def __call__(self, img):
-        random_value = np.random.random()  # random.random()
-        # print(random_value)
-        if random_value > self.probability:
+
+        if random.uniform(0, 1) > self.probability:
             return img
-        # count = 0
+
         for attempt in range(100):
             area = img.size()[1] * img.size()[2]
-
-            target_area = np.random.uniform(self.sl, self.sh) * area
-            aspect_ratio = np.random.uniform(self.r1, 1 / self.r1)
+       
+            target_area = random.uniform(self.sl, self.sh) * area
+            aspect_ratio = random.uniform(self.r1, 1/self.r1)
 
             h = int(round(math.sqrt(target_area * aspect_ratio)))
             w = int(round(math.sqrt(target_area / aspect_ratio)))
 
             if w < img.size()[2] and h < img.size()[1]:
-                # count +=1
-                x1 = np.random.randint(0, img.size()[1] - h)
-                y1 = np.random.randint(0, img.size()[2] - w)
+                x1 = random.randint(0, img.size()[1] - h)
+                y1 = random.randint(0, img.size()[2] - w)
                 if img.size()[0] == 3:
-                    img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
-                    img[1, x1:x1 + h, y1:y1 + w] = self.mean[1]
-                    img[2, x1:x1 + h, y1:y1 + w] = self.mean[2]
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
+                    img[1, x1:x1+h, y1:y1+w] = self.mean[1]
+                    img[2, x1:x1+h, y1:y1+w] = self.mean[2]
                 else:
-                    img[0, x1:x1 + h, y1:y1 + w] = self.mean[0]
+                    img[0, x1:x1+h, y1:y1+w] = self.mean[0]
                 return img
+
         return img
 
 class Dataloder():
@@ -85,9 +130,9 @@ class Dataloder():
             transforms.RandomHorizontalFlip(),
             # transforms.ColorJitter(0.2,0.2,0.2),
             transforms.ToTensor(),
-            # Lighting(0.1, _imagenet_pca['eigval'], _imagenet_pca['eigvec']),
+            Lighting(0.1, _imagenet_pca['eigval'], _imagenet_pca['eigvec']),
             normalize,
-            RandomErasing(mean=[0.0, 0.0, 0.0]),
+            RandomErasing(),
         ])
         transform_test = transforms.Compose([
             # transforms.Resize(256),
